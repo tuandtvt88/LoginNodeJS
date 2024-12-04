d54638c33ecee9ebd01b096213f944834cc24701
tuandtvt88 <tuandtvt88>
2024-12-04 22:32:40 +0700

    lan2

---
+++ src/containers/Auth/Login.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { push } from "connected-react-router";
+
+import * as actions from "../../store/actions";
+
+import './Login.scss';
+
+
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+   
+    render() {
+        return (
+<div className='login-background'>
+    <div className='login-container'>
+        <div className='login-content'>
+            abc
+        </div>
+        </div>
+</div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        language: state.app.language
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        navigate: (path) => dispatch(push(path)),
+        adminLoginSuccess: (adminInfo) => dispatch(actions.adminLoginSuccess(adminInfo)),
+        adminLoginFail: () => dispatch(actions.adminLoginFail()),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
