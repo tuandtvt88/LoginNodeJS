8d75a7144994093d726bde5c22eef71a1677336e
tuandtvt88 <tuandtvt88>
2024-12-05 22:35:56 +0700

    lan4

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -19,8 +19,27 @@ class Login extends Component {
         return (
 <div className='login-background'>
     <div className='login-container'>
-        <div className='login-content'>
-            abc
+        <div className='login-content row'>
+            <div className='col-12 text-login'>Login</div>
+            <div className='col-12 form-group login-input'>
+                <label>Username:</label>
+                <input type='text' className='form-control' placeholder='Enter your username'></input>
+            </div>
+            <div className='col-12 form-group login-input'>
+                <label>Password:</label>
+                <input type='text' className='form-control' placeholder='Enter your password'></input>
+            </div>
+            <div className='col-12'>
+            <button className='btn-login'>Login</button>
+            </div>
+            
+            <div className='col-12'>
+                <span className='forgot-password'>Forgot your Password?</span>
+            </div>
+            <div className='col-12 text-center'>
+                <span className='text-center'>Or Login with:</span>
+            </div>
+            <div className='col-12 social-login'></div>
         </div>
         </div>
 </div>
