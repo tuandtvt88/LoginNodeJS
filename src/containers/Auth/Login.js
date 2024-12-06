36ccaf965424297e647cabf36555efbc16489390
tuandtvt88 <tuandtvt88@gmail.com>
2024-12-06 17:02:01 +0700

    lan5

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -11,38 +11,60 @@ import './Login.scss';
 class Login extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
 
     }
 
-   
+    handleOnChangeUsername = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+        console.log(event.target.value);
+    }
+    handleOnChangePassword = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+        console.log(event.target.value);
+    }
     render() {
         return (
-<div className='login-background'>
-    <div className='login-container'>
-        <div className='login-content row'>
-            <div className='col-12 text-login'>Login</div>
-            <div className='col-12 form-group login-input'>
-                <label>Username:</label>
-                <input type='text' className='form-control' placeholder='Enter your username'></input>
+            <div className='login-background'>
+                <div className='login-container'>
+                    <div className='login-content row'>
+                        <div className='col-12 text-login'>Login</div>
+                        <div className='col-12 form-group login-input'>
+                            <label>Username:</label>
+                            <input type='text' className='form-control' placeholder='Enter your username'
+                                value={this.state.username}
+                                onChange={(event) => this.handleOnChangeUsername(event)}></input>
+                        </div>
+                        <div className='col-12 form-group login-input'>
+                            <label>Password:</label>
+                            <input type='text' className='form-control' placeholder='Enter your password'
+                                value={this.state.password}
+                                onChange={(event) => this.handleOnChangePassword(event)}></input>
+                        </div>
+                        <div className='col-12'>
+                            <button className='btn-login'>Login</button>
+                        </div>
+
+                        <div className='col-12'>
+                            <span className='forgot-password'>Forgot your Password?</span>
+                        </div>
+                        <div className='col-12 text-center mt-3'>
+                            <span className='text-other-login'>Or Login with:</span>
+                        </div>
+                        <div className='col-12 social-login'>
+                            <i className="fab fa-google-plus-g google"></i>
+                            <i className="fab fa-facebook-f facebook"></i>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className='col-12 form-group login-input'>
-                <label>Password:</label>
-                <input type='text' className='form-control' placeholder='Enter your password'></input>
-            </div>
-            <div className='col-12'>
-            <button className='btn-login'>Login</button>
-            </div>
-            
-            <div className='col-12'>
-                <span className='forgot-password'>Forgot your Password?</span>
-            </div>
-            <div className='col-12 text-center'>
-                <span className='text-center'>Or Login with:</span>
-            </div>
-            <div className='col-12 social-login'></div>
-        </div>
-        </div>
-</div>
         )
     }
 }
